536d2cc38f43e707c62facfc71c9d3f8652a9a12
Madhusudan Gurjar <161649945+Madhusudan-Gurjar@users.noreply.github.com>
2025-08-16 20:47:04 +0530

    Update App.js
    cors update

--- Backend/App.js
+++ Backend/App.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+// ✅ Allow only your Netlify frontend
+app.use(cors({
+  origin: "https://ai-powered-video-platform.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB connection
